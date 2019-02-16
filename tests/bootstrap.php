6b2bbb392b9a7d7c8df12b7b6773555fce4c7e4b
Thomas Nunninger <thomas.nunninger@scaleupventures.com>
2019-02-16 20:18:11 +0100

    Initial commit.

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types = 1);
+
+/**
+ * This file is part of ScaleUpStack/Assert.
+ *
+ * For the full copyright and license information, please view the README.md and LICENSE.md files that were distributed
+ * with this source code.
+ *
+ * @copyright 2019 ScaleUpVentures GmbH, https://www.scaleupventures.com
+ * @link      https://github.com/scaelupstack/assert
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
